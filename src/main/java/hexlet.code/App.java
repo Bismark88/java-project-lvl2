223a6e652c5962274f47b498e9476221f40402fc
Kirill_Bismark <project1326@mail.ru>
2022-06-02 00:24:38 +1000

    Stage 3. Project configuration lvl2. Add file Differ.java

--- src/main/java/hexlet.code/App.java
+++ src/main/java/hexlet.code/App.java
@@ -1,7 +1,26 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares the two configuration files and shows the difference between them.")
+
+public class App implements Runnable {
+
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
+    private boolean helpRequested = false;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean versionInfoRequested;
+
+    @Override
+    public void run() {
+        System.out.println("Hello, world!");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
